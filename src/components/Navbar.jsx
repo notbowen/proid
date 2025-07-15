8f2d98b940c72653cf989c7ba9f4618dec9cce38
notbowen <contact@hubowen.dev>
2025-07-15 23:42:50 +0800

    fix navbar padding

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -33,8 +33,8 @@ const Navbar = () => {
                 bg={useColorModeValue('white', 'gray.800')}
                 color={useColorModeValue('gray.600', 'white')}
                 minH={'60px'}
-                py={{ base: 2 }}
-                px={{ base: 4 }}
+                py={{ base: 4 }}
+                px={{ base: 10, md: 12, lg: 16 }}
                 borderBottom={1}
                 borderStyle={'solid'}
                 borderColor={useColorModeValue('gray.200', 'gray.900')}
