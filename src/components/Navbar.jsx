3b00a0219f55f540410fe7f178defc9a9627464c
notbowen <contact@hubowen.dev>
2025-07-15 23:44:55 +0800

    add subpages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -161,12 +161,12 @@ const DesktopSubNav = ({ label, href, subLabel }) => {
             display={'block'}
             p={2}
             rounded={'md'}
-            _hover={{ bg: useColorModeValue('pink.50', 'gray.900') }}>
+            _hover={{ bg: useColorModeValue('red.50', 'gray.900') }}>
             <Stack direction={'row'} align={'center'}>
                 <Box>
                     <Text
                         transition={'all .3s ease'}
-                        _groupHover={{ color: 'pink.400' }}
+                        _groupHover={{ color: 'red.400' }}
                         fontWeight={500}>
                         {label}
                     </Text>
@@ -180,7 +180,7 @@ const DesktopSubNav = ({ label, href, subLabel }) => {
                     justify={'flex-end'}
                     align={'center'}
                     flex={1}>
-                    <Icon color={'pink.400'} w={5} h={5} as={ChevronRightIcon} />
+                    <Icon color={'red.400'} w={5} h={5} as={ChevronRightIcon} />
                 </Flex>
             </Stack>
         </Box>
@@ -267,8 +267,13 @@ const NAV_ITEMS = [
                 href: '#',
             },
             {
+                label: 'Suggest',
+                subLabel: 'Send in your suggestion to URA!',
+                href: '#',
+            },
+            {
                 label: 'Progress Tracker',
-                subLabel: 'Track your feedback progress',
+                subLabel: 'Track your suggestion\'s progress',
                 href: '#',
             },
         ],
